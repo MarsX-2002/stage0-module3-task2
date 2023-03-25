cc506a5355035cf21bc3a055e63a9b0c0a1f7f80
MarsX-2002 <mirjalol0331@gmail.com>
2023-03-25 08:37:00 +0500

    naming convention

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,14 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable = 10;
-        int variableOne = 1;
-        int variableTwo = 2;
-        int variableThree = 3;
+        int _variable = 10;
+        int variable1 = 1;
+        int variable2 = 2;
+        int variable3 = 3;
         final int VARIABLE_FOUR = 4;
         final int VARIABLE_FIVE = 5;
-        int variableSix = 6;
-        int variableSeven = 7;
-        int variableEight = 8;
+        int variable6 = 6;
+        int variable7 = 7;
+        int variable8 = 8;
     }
 }
